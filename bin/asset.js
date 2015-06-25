c5a0707ebb9cefcbebb13f499efa55071ca99b4d
Assaf Shomer <assafshomer@gmail.com>
2015-06-25 16:15:25 +0300

    fixed index

--- bin/asset.js
+++ bin/asset.js
@@ -4,10 +4,10 @@ var fs = require('fs');
 
 assetid = 'LgcDSqEnNGNdZPvBA8eGNaFUXA6Yjms7YZ';
 txid='8ddaf1f77a38d7dc0a9ceb787f217f5d153a9a8eced2dd836ed04944dfcaf076'
-index = '0'
+index = '2'
 
 function getFromApi(api_endpoint, data, callback) {
-	console.log('Get from:'+api_endpoint+'/', data);
+	console.log('Get from:'+api_endpoint+'/'+data);
 	request.get('http://api.coloredcoins.org:80/v2/'+api_endpoint+'/'+data, function (error, response, body) {
 	    if (error) {
 	        return callback(error);
@@ -17,6 +17,12 @@ function getFromApi(api_endpoint, data, callback) {
 	    }
 	    console.log('Status:', response.statusCode);
 	    console.log('Body:', body);
+		fs.writeFile('./log/'+api_endpoint+'_'+Date.now()+'.json', body, function(err) {
+		    if(err) {
+		        return console.log(err);
+		    }
+		    console.log("The file was saved!");
+		});		    
 	  	return callback(null, body);
 	});
 };
@@ -35,9 +41,9 @@ getFromApi('assetmetadata',assetid+'/'+txid+':'+index,function(err, body){
 //   if (err) consule.log('error: '+err);
 // });
 
-getFromApi('stakeholders',assetid+'/0',function(err, body){
-  if (err) consule.log('error: '+err);
-});
+// getFromApi('stakeholders',assetid+'/0',function(err, body){
+//   if (err) consule.log('error: '+err);
+// });
 
 // getFromApi('addressinfo',assetid,function(err, body){
 //   if (err) consule.log('error: '+err);
